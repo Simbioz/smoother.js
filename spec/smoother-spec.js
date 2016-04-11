c661bdcd01b2f1ca3af89cad9fca0a1a46c6a66d
Michaël Fortin <michael@simbioz.com>
2016-04-11 13:59:53 -0400

    Add tests for the supported weighting distributions

--- spec/smoother-spec.js
+++ spec/smoother-spec.js
@@ -3,19 +3,57 @@ var Smoother = require("../dist/smoother.min.js");
 describe("Smoother", function () {
   describe("values", function () {
     it("cannot contain more than [max values] values", function () {
+      var maxValueCount = 2;
+      var smoother = new Smoother(maxValueCount, "cubic");
+      smoother.push(1);
+      smoother.push(3);
+      smoother.push(9);
+      expect(smoother.values.length).toEqual(maxValueCount);
+    });
+
+    it("should properly calculate value with linear weight distribution", function () {
+      var maxValueCount = 4;
+      var smoother = new Smoother(maxValueCount, "linear");
+      smoother.push(1);
+      expect(smoother.value).toEqual(1);
+      smoother.push(3);
+      expect(smoother.value).toEqual(2.3333333333333335);
+      smoother.push(9);
+      expect(smoother.value).toEqual(5.666666666666667);
+      smoother.push(15);
+      expect(smoother.value).toEqual(9.4);
+      smoother.push(19);
+      expect(smoother.value).toEqual(14.2);
+    });
+
+    it("should properly calculate value with quadratic weight distribution", function () {
+      var maxValueCount = 4;
+      var smoother = new Smoother(maxValueCount, "quadratic");
+      smoother.push(1);
+      expect(smoother.value).toEqual(1);
+      smoother.push(3);
+      expect(smoother.value).toEqual(2.6);
+      smoother.push(9);
+      expect(smoother.value).toEqual(6.714285714285714);
+      smoother.push(15);
+      expect(smoother.value).toEqual(11.133333333333333);
+      smoother.push(19);
+      expect(smoother.value).toEqual(15.933333333333334);
+    });
+
+    it("should properly calculate value with cubic weight distribution", function () {
       var maxValueCount = 4;
       var smoother = new Smoother(maxValueCount, "cubic");
       smoother.push(1);
-      console.log(smoother.value);
+      expect(smoother.value).toEqual(1);
       smoother.push(3);
-      console.log(smoother.value);
+      expect(smoother.value).toEqual(2.7777777777777777);
       smoother.push(9);
-      console.log(smoother.value);
+      expect(smoother.value).toEqual(7.444444444444445);
       smoother.push(15);
-      console.log(smoother.value);
+      expect(smoother.value).toEqual(12.28);
       smoother.push(19);
-      console.log(smoother.value);
-      expect(smoother.values.length).toEqual(maxValueCount);
+      expect(smoother.value).toEqual(16.96);
     });
   });
 });
